d80b7028e6bc65276be588dca760526a684272c1
theAndrewCline <andrew.cline77@gmail.com>
2018-03-30 15:46:30 -0500

    scrolling fixed, react done

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react'
 
 export default class Nav extends Component {
+	constructor(props) {
+		super(props)
+	}
+
+	scrolling(event) {
+		let destinationId = event.target.getAttribute('id')
+		let destination = document.querySelector(destinationId)
+
+		destination.scrollIntoView({ behavior: 'smooth' })
+	}
+
 	render() {
 		return (
 			<nav>
-				<a id="#world-premiere">World Premiere</a>
-				<a id="#the-composer-anchor">Composer</a>
-				<a id="#story-anchor">Story</a>
-				<a id="#cast-anchor">Cast</a>
-				<a id="#songs-anchor">Songs</a>
+				<a onClick={this.scrolling} id="#world-premiere">
+					World Premiere
+				</a>
+				<a onClick={this.scrolling} id="#the-composer-anchor">
+					Composer
+				</a>
+				<a onClick={this.scrolling} id="#story-anchor">
+					Story
+				</a>
+				<a onClick={this.scrolling} id="#cast-anchor">
+					Cast
+				</a>
+				<a onClick={this.scrolling} id="#songs-anchor">
+					Songs
+				</a>
 			</nav>
 		)
 	}
